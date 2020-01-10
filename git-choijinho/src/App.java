def68adbbd1b51692855a0fd084325d986ce63db
goott6 <goott6@goott6-PC>
2020-01-10 15:50:20 +0900

    test1234

--- git-choijinho/src/App.java
+++ git-choijinho/src/App.java
@@ -6,7 +6,7 @@ public class App {
 		System.out.println("누가 먼jkjk저 할까요????"); // 조승연 작성
 		// TODO Auto-generated method stub
 		System.out.println("jkjk");// 최진호 작성		
-		System.out.println("test");
+		System.out.println("test1234");
 	}
 
 }

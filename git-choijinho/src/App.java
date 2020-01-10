45247c88685a0668d99411125390692e9ff7e14c
goott6 <goott6@goott6-PC>
2020-01-10 15:30:39 +0900

    254

--- git-choijinho/src/App.java
+++ git-choijinho/src/App.java
@@ -4,7 +4,7 @@ public class App {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("test");// 최진호 작성
-		System.out.println("누가 먼저 할까요?");
+		System.out.println("구트");
 
 	}
 

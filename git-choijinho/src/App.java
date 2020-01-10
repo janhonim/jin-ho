a598d23f2b5434fc08505c167967bf3f66d91988
goott6 <goott6@goott6-PC>
2020-01-10 15:49:02 +0900

    UpDate Code - SY

--- git-choijinho/src/App.java
+++ git-choijinho/src/App.java
@@ -5,9 +5,7 @@ public class App {
 		// TODO Auto-generated method stub
 		System.out.println("누가 먼jkjk저 할까요????"); // 조승연 작성
 		// TODO Auto-generated method stub
-		System.out.println("jkjk");// 최진호 작성
-		
-		
+		System.out.println("jkjk");// 최진호 작성		
 		System.out.println("test");
 	}
 

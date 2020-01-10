7bbc604c632ffa873c33601546fc71c50da38002
goott6 <goott6@goott6-PC>
2020-01-10 15:16:44 +0900

    update code -sy

--- git-choijinho/src/App.java
+++ git-choijinho/src/App.java
@@ -3,7 +3,7 @@ public class App {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("누가 먼저 할까요???"); // 조승연 작성
+		System.out.println("누가 먼저 할까요????"); // 조승연 작성
 	}
 
 }

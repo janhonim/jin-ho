9105796cc9c55e668a0069dc5299da8f0dc97586
goott6 <goott6@goott6-PC>
2020-01-10 15:35:34 +0900

    update codecode

--- git-choijinho/src/App.java
+++ git-choijinho/src/App.java
@@ -6,7 +6,6 @@ public class App {
 		System.out.println("누가 먼jkjk저 할까요????"); // 조승연 작성
 		// TODO Auto-generated method stub
 		System.out.println("jkjk");// 최진호 작성
-		System.out.println("jkjk");
 	}
 
 }
